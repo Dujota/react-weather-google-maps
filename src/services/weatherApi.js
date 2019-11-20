7d4fe4a2f88f80454d76ac7f182215c8317d318e
Denis <ddujota@internationalsafety.com>
2019-11-19 23:59:01 -0500

    updated the weather api to use keys.ks

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,6 +1,7 @@
+import { weatherAPI } from '../../keys'
 
 export function getCurWeatherByLatLng(lat, lng) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=d3945aa316355ce92bb8cc10bf63e3da`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${weatherAPI}`
 
   return fetch( url, {mode:'cors'}).then(response => response.json())
  }
